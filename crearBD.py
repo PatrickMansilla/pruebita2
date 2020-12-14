c2f3bc5fab1e07295eb198368df68b6087dca695
iscojara2020 <iscojara14@gmail.com>
2020-12-14 01:15:38 -0500

    ryg

---
+++ crearBD.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 20:20:59 2020
+
+@author: osito
+"""
+
+
+# Creamos BD Biblioteca
+
+import sqlite3
+conexion = sqlite3.connect("bdprueba.db")
+conexion.close()
